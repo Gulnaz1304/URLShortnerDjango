5f985c8634c7b2497bb206959ab8318c61efcfa9
Gulnaz Amirkhanova <mrrrrrrrrrrrrr1@icloud.com>
2021-05-13 16:12:58 +0300

    trying some shortening

--- shortener/models.py
+++ shortener/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+import random
+import string
+
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    new_code = ''
+    for i in range(size):
+        new_code += random.choice(chars)
+    return new_code
 
 
 class ShortenedURL(models.Model):
@@ -6,6 +15,11 @@ class ShortenedURL(models.Model):
     shortcode = models.CharField(max_length=15, unique=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        super(ShortenedURL, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return str(self.url)
