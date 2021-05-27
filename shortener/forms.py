c7b8613d9561393ae1bf86359ad6355574c66067
Gulnaz Amirkhanova <mrrrrrrrrrrrrr1@icloud.com>
2021-05-27 18:03:24 +0300

    bootstrap added

--- shortener/forms.py
+++ shortener/forms.py
@@ -4,7 +4,12 @@ from django.core.exceptions import ValidationError
 from .validators import validate_url, validate_dot_com
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(label='Submit url', validators=[validate_url, validate_dot_com])
+    url = forms.CharField(label='',
+                          validators=[validate_url],
+                          widget=forms.TextInput(
+                              attrs={"placeholder": "Long URL",
+                                     "class": "form-control"}
+                          ))
 
     # def clean(self):
     #     cleaned_data = super(SubmitUrlForm, self).clean()
